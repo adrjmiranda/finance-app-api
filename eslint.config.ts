120aaf569e0b10ed9e7a9e15441ef6ffa82e5e7f
Adriano Miranda <adrjmiranda@gmail.com>
2026-03-13 11:59:58 -0400

    chore: setup typescript eslint prettier and path aliases

---
+++ eslint.config.ts
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+	{
+		ignores: ['node_modules', 'dist'],
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+		plugins: { js },
+		extends: ['js/recommended'],
+		languageOptions: { globals: globals.node },
+		rules: {
+			'no-console': 'off',
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+			],
+			'@typescript-eslint/no-explicit-any': 'error',
+		},
+	},
+	tseslint.configs.recommended,
+	eslintConfigPrettier,
+]);
