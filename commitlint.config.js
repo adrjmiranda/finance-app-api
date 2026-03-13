1535b088ef7d27b7a8b8d8aad503a17c4ddc261c
Adriano Miranda <adrjmiranda@gmail.com>
2026-03-13 12:03:20 -0400

    chore: add husky commitlint and lint-staged

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+export default {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'subject-case': [2, 'always', 'lower-case'],
+		'subject-full-stop': [2, 'never', '.'],
+		'header-max-length': [2, 'always', 70],
+	},
+};
