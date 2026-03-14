66028c7cf1bb50c40c7c92aebd4bf670c421c659
Adriano Miranda <adrjmiranda@gmail.com>
2026-03-13 22:06:23 -0400

    feat: initialize fastify server with env configuration

--- src/shared/env/schema.ts
+++ src/shared/env/schema.ts
@@ -3,5 +3,6 @@ import * as z from 'zod';
 export const envSchema = z.object({
 	SERVER_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
 	SERVER_PORT: z.coerce.number().default(3333),
+	SERVER_HOST: z.string().default('0.0.0.0'),
 	DATABASE_URL: z.url(),
 });
