2bd2cd541fa80897e9fa67018defcf37c0631fd8
Adriano Miranda <adrjmiranda@gmail.com>
2026-03-18 12:03:06 -0300

    test: add integration test for deleteuserprofileservice

---
+++ src/modules/users/services/postgres/DeleteUserProfileService/DeleteUserProfileService.test.ts
@@ -0,0 +1,59 @@
+import 'reflect-metadata';
+
+import { describe, test, beforeEach } from 'node:test';
+import assert from 'node:assert';
+import { DeleteUserProfileService } from './DeleteUserProfileService.js';
+import { container } from 'tsyringe';
+
+import bcrypt from 'bcrypt';
+import { db } from '#/shared/infra/database/drizzle/db.js';
+import { usersTable } from '#/shared/infra/database/drizzle/schemas/users.js';
+import { eq } from 'drizzle-orm';
+
+describe('DeleteUserProfileService (Integration)', async () => {
+	let deleteUserProfileService: DeleteUserProfileService;
+
+	beforeEach(async () => {
+		await db.delete(usersTable);
+
+		const childContainer = container.createChildContainer();
+		deleteUserProfileService = childContainer.resolve(DeleteUserProfileService);
+	});
+
+	test('should successfully delete the user profile', async () => {
+		const userData = {
+			firstName: 'Adriano',
+			lastName: 'Miranda',
+			email: 'delete@test.com',
+			password: 'password123',
+		};
+
+		const passwordHash = await bcrypt.hash(userData.password, 10);
+
+		const [createdUser] = await db
+			.insert(usersTable)
+			.values({
+				firstName: userData.firstName,
+				lastName: userData.lastName,
+				email: userData.email,
+				passwordHash,
+			})
+			.returning()
+			.execute();
+
+		await assert.doesNotReject(async () => {
+			await deleteUserProfileService.execute({
+				userId: createdUser?.id ?? 'not-found',
+				password: userData.password,
+			});
+		});
+
+		const [deletedUser] = await db
+			.select()
+			.from(usersTable)
+			.where(eq(usersTable.id, createdUser?.id ?? 'not-found'))
+			.limit(1);
+
+		assert.ok(!deletedUser?.id);
+	});
+});
