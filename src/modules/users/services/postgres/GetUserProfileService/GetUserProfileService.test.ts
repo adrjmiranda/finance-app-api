cf67c63078407c74e52058e1149aa66b01de8ff1
Adriano Miranda <adrjmiranda@gmail.com>
2026-03-18 12:12:09 -0300

    test: add integration test for getuserprofileservice

---
+++ src/modules/users/services/postgres/GetUserProfileService/GetUserProfileService.test.ts
@@ -0,0 +1,52 @@
+import 'reflect-metadata';
+
+import { describe, test, beforeEach } from 'node:test';
+import assert from 'node:assert';
+import { GetUserProfileService } from './GetUserProfileService.js';
+import { db } from '#/shared/infra/database/drizzle/db.js';
+import { usersTable } from '#/shared/infra/database/drizzle/schemas/users.js';
+import { container } from 'tsyringe';
+
+import bcrypt from 'bcrypt';
+
+describe('GetUserProfileService (Integration)', async () => {
+	let getUserProfileService: GetUserProfileService;
+
+	beforeEach(async () => {
+		await db.delete(usersTable);
+
+		const childContainer = container.createChildContainer();
+		getUserProfileService = childContainer.resolve(GetUserProfileService);
+	});
+
+	test('should successfully get the user profile', async () => {
+		const userData = {
+			firstName: 'Adriano',
+			lastName: 'Miranda',
+			email: 'userprofile@test.com',
+			password: 'password123',
+		};
+
+		const passwordHash = await bcrypt.hash(userData.password, 10);
+
+		const [createdUser] = await db
+			.insert(usersTable)
+			.values({
+				firstName: userData.firstName,
+				lastName: userData.lastName,
+				email: userData.email,
+				passwordHash,
+			})
+			.returning()
+			.execute();
+
+		const { user } = await getUserProfileService.execute({
+			userId: createdUser?.id ?? 'not-found',
+		});
+
+		assert.ok(user?.id);
+		assert.strictEqual(user.firstName, userData.firstName);
+		assert.strictEqual(user.lastName, userData.lastName);
+		assert.strictEqual(user.email, userData.email);
+	});
+});
