e9b4d500e7df9f205f98555018f3ac62bcbf0709
Adriano Miranda <adrjmiranda@gmail.com>
2026-03-23 19:09:39 -0300

    test: add integration tests for getuserprofilecontroller

---
+++ src/modules/users/infra/http/controllers/GetUserProfileController/GetUserProfileController.test.ts
@@ -0,0 +1,71 @@
+import 'reflect-metadata';
+
+import { describe, test, beforeEach } from 'node:test';
+import assert from 'node:assert';
+import { db } from '#/shared/infra/database/drizzle/db.js';
+import { usersTable } from '#/shared/infra/database/drizzle/schemas/users.js';
+import { createAndAuthenticateUser } from '#/shared/utils/authenticate-user-helper.js';
+import { app } from '#/shared/infra/http/app.js';
+import { randomUUID } from 'node:crypto';
+import { ERROR_CODES } from '#/shared/constants/errors/codes/codes.js';
+
+describe('GetUserProfileController (Integration)', () => {
+	beforeEach(async () => {
+		await db.delete(usersTable);
+	});
+
+	test('should get the user profile', async () => {
+		const { token, authenticatedUser } = await createAndAuthenticateUser(app);
+
+		const response = await app.inject({
+			method: 'GET',
+			url: '/users/me',
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const body = JSON.parse(response.payload);
+
+		assert.strictEqual(response.statusCode, 200);
+		assert.strictEqual(body.user.id, authenticatedUser?.id);
+		assert.strictEqual(body.user.firstName, authenticatedUser?.firstName);
+		assert.strictEqual(body.user.lastName, authenticatedUser?.lastName);
+		assert.strictEqual(body.user.email, authenticatedUser?.email);
+	});
+
+	test('should throw an error if user is not authenticated', async () => {
+		const { token } = await createAndAuthenticateUser(app);
+
+		const response = await app.inject({
+			method: 'GET',
+			url: '/users/me',
+			headers: {
+				Authorization: `Bearer INVALID-TOKEN-${token}`,
+			},
+		});
+
+		assert.strictEqual(response.statusCode, 401);
+	});
+
+	test('should throw an error if user not found', async () => {
+		const token = app.jwt.sign({
+			sub: randomUUID(),
+		});
+
+		const response = await app.inject({
+			method: 'GET',
+			url: '/users/me',
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const body = JSON.parse(response.payload);
+
+		assert.strictEqual(response.statusCode, 404);
+		assert.deepStrictEqual(body, {
+			code: ERROR_CODES.USER_NOT_FOUND,
+		});
+	});
+});
