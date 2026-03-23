7c87faf2ba7e3beeb1930af30af392eb8b36e3a4
Adriano Miranda <adrjmiranda@gmail.com>
2026-03-22 23:17:26 -0300

    test: add integration tests for authenticateusercontroller

---
+++ src/modules/users/infra/http/controllers/AuthenticateUserController/AuthenticateUserController.test.ts
@@ -0,0 +1,103 @@
+import 'reflect-metadata';
+
+import { describe, test, beforeEach } from 'node:test';
+import assert from 'node:assert';
+import { db } from '#/shared/infra/database/drizzle/db.js';
+import { usersTable } from '#/shared/infra/database/drizzle/schemas/users.js';
+
+import bcrypt from 'bcrypt';
+import { app } from '#/shared/infra/http/app.js';
+import { ERROR_CODES } from '#/shared/constants/errors/codes/codes.js';
+
+describe('AuthenticateUserController (Integration)', () => {
+	beforeEach(async () => {
+		await db.delete(usersTable);
+	});
+
+	test('should authenticate a user', async () => {
+		const userPayload = {
+			email: 'adriano@email.com',
+			password: 'password123',
+		};
+
+		const passwordHash = await bcrypt.hash(userPayload.password, 10);
+
+		const [createdUser] = await db
+			.insert(usersTable)
+			.values({
+				firstName: 'Adriano',
+				lastName: 'Miranda',
+				email: userPayload.email,
+				passwordHash,
+			})
+			.returning()
+			.execute();
+
+		const response = await app.inject({
+			method: 'POST',
+			url: '/users/sessions',
+			payload: userPayload,
+		});
+
+		assert.strictEqual(response.statusCode, 200);
+
+		const body = JSON.parse(response.payload);
+
+		assert.strictEqual(body.user.id, createdUser?.id);
+		assert.strictEqual(body.user.firstName, createdUser?.firstName);
+		assert.strictEqual(body.user.lastName, createdUser?.lastName);
+		assert.strictEqual(body.user.email, createdUser?.email);
+		assert.ok(body.token);
+	});
+
+	test('should throw an error if user does not exist', async () => {
+		const userPayload = {
+			email: 'notfound@email.com',
+			password: 'password123',
+		};
+
+		const response = await app.inject({
+			method: 'POST',
+			url: '/users/sessions',
+			payload: userPayload,
+		});
+
+		assert.strictEqual(response.statusCode, 404);
+
+		const body = JSON.parse(response.payload);
+
+		assert.strictEqual(body.code, ERROR_CODES.USER_NOT_FOUND);
+	});
+
+	test('should throw an error if password is incorrect', async () => {
+		const userPayload = {
+			email: 'adriano@email.com',
+			password: 'wrong-password',
+		};
+
+		const passwordHash = await bcrypt.hash('correct-password', 10);
+
+		await db
+			.insert(usersTable)
+			.values({
+				firstName: 'Adriano',
+				lastName: 'Miranda',
+				email: userPayload.email,
+				passwordHash,
+			})
+			.returning()
+			.execute();
+
+		const response = await app.inject({
+			method: 'POST',
+			url: '/users/sessions',
+			payload: userPayload,
+		});
+
+		assert.strictEqual(response.statusCode, 401);
+
+		const body = JSON.parse(response.payload);
+
+		assert.strictEqual(body.code, ERROR_CODES.INVALID_CREDENTIALS);
+	});
+});
