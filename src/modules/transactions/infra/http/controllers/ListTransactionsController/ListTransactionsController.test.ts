4db018aa752bcc0fc5d0bc3cc30162c8af7637af
Adriano Miranda <adrjmiranda@gmail.com>
2026-03-25 17:19:12 -0300

    test: add e2e tests for listtransactionscontroller

--- src/modules/transactions/infra/http/controllers/ListTransactionsController/ListTransactionsController.test.ts
+++ src/modules/transactions/infra/http/controllers/ListTransactionsController/ListTransactionsController.test.ts
@@ -4,50 +4,54 @@ import { describe, test, beforeEach } from 'node:test';
 import assert from 'node:assert';
 import { db } from '#/shared/infra/database/drizzle/db.js';
 import { usersTable } from '#/shared/infra/database/drizzle/schemas/users.js';
-import { transactionsTable } from '#/shared/infra/database/drizzle/schemas/transactions.js';
 import { createAndAuthenticateUser } from '#/shared/utils/authenticate-user-helper.js';
 import { app } from '#/shared/infra/http/app.js';
+import {
+	TRANSACTION_TYPES,
+	transactionsTable,
+} from '#/shared/infra/database/drizzle/schemas/transactions.js';
 
-describe('GetTransactionsBalanceController (Integration)', () => {
+describe('ListTransactionsController (Integration)', () => {
 	beforeEach(async () => {
 		await db.delete(usersTable);
 	});
 
-	test('should return the transactions balance', async () => {
+	test('should list transactions', async () => {
 		const { authenticatedUser, token } = await createAndAuthenticateUser(app);
 
 		const earning = 150.0;
 		const expense = 62.0;
 		const investment = 220.0;
-		const balance = earning - expense + investment;
 
-		await db.insert(transactionsTable).values([
+		const transactionsData = [
 			{
-				name: 'Pizza',
+				name: 'Transaction 1',
 				date: new Date(),
 				amount: String(expense),
-				type: 'expense',
+				type: 'expense' as (typeof TRANSACTION_TYPES)[number],
 				userId: authenticatedUser!.id,
 			},
 			{
-				name: 'Pizza',
+				name: 'Transaction 2',
 				date: new Date(),
 				amount: String(earning),
-				type: 'earning',
+				type: 'earning' as (typeof TRANSACTION_TYPES)[number],
 				userId: authenticatedUser!.id,
 			},
 			{
-				name: 'Pizza',
+				name: 'Transaction 3',
 				date: new Date(),
 				amount: String(investment),
-				type: 'investment',
+				type: 'investment' as (typeof TRANSACTION_TYPES)[number],
 				userId: authenticatedUser!.id,
 			},
-		]);
+		];
+
+		await db.insert(transactionsTable).values(transactionsData);
 
 		const response = await app.inject({
 			method: 'GET',
-			url: '/transactions/balances',
+			url: '/transactions',
 			headers: {
 				Authorization: `Bearer ${token}`,
 			},
@@ -57,11 +61,19 @@ describe('GetTransactionsBalanceController (Integration)', () => {
 
 		const body = JSON.parse(response.payload);
 
-		assert.deepStrictEqual(body, {
-			earnings: earning,
-			expenses: expense,
-			investments: investment,
-			balance,
-		});
+		assert.strictEqual(body.transactions.length, transactionsData.length);
+
+		assert.deepStrictEqual(
+			body.transactions[0].name,
+			transactionsData[0]?.name
+		);
+		assert.deepStrictEqual(
+			body.transactions[1].name,
+			transactionsData[1]?.name
+		);
+		assert.deepStrictEqual(
+			body.transactions[2].name,
+			transactionsData[2]?.name
+		);
 	});
 });
