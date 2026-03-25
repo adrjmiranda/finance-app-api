8462b594d74c0c0958f0751095f656dbb3b2c38c
Adriano Miranda <adrjmiranda@gmail.com>
2026-03-25 16:40:09 -0300

    test: add e2e tests for gettransactioncontroller

---
+++ src/modules/transactions/infra/http/controllers/GetTransactionController/GetTransactionController.test.ts
@@ -0,0 +1,71 @@
+import 'reflect-metadata';
+
+import { describe, test, beforeEach } from 'node:test';
+import assert from 'node:assert';
+import { db } from '#/shared/infra/database/drizzle/db.js';
+import { usersTable } from '#/shared/infra/database/drizzle/schemas/users.js';
+import { createAndAuthenticateUser } from '#/shared/utils/authenticate-user-helper.js';
+import { app } from '#/shared/infra/http/app.js';
+import { transactionsTable } from '#/shared/infra/database/drizzle/schemas/transactions.js';
+import { randomUUID } from 'node:crypto';
+import { ERROR_CODES } from '#/shared/constants/errors/codes/codes.js';
+
+describe('GetTransactionController (Integration)', () => {
+	beforeEach(async () => {
+		await db.delete(usersTable);
+	});
+
+	test('should get a transaction', async () => {
+		const { authenticatedUser, token } = await createAndAuthenticateUser(app);
+
+		const [createdTransaction] = await db
+			.insert(transactionsTable)
+			.values({
+				name: 'Pizza',
+				date: new Date(),
+				amount: String(62.0),
+				type: 'expense',
+				userId: authenticatedUser!.id,
+			})
+			.returning()
+			.execute();
+
+		const transactionId = createdTransaction!.id;
+
+		const response = await app.inject({
+			method: 'GET',
+			url: `/transactions/${transactionId}`,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const body = JSON.parse(response.payload);
+
+		assert.strictEqual(response.statusCode, 200);
+
+		assert.ok(body.transaction);
+
+		assert.strictEqual(body.transaction.id, transactionId);
+		assert.strictEqual(body.transaction.userId, authenticatedUser!.id);
+		assert.strictEqual(body.transaction.name, 'Pizza');
+	});
+
+	test('should throw an error if transaction does not exist', async () => {
+		const { token } = await createAndAuthenticateUser(app);
+
+		const response = await app.inject({
+			method: 'GET',
+			url: `/transactions/${randomUUID()}`,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		assert.strictEqual(response.statusCode, 404);
+
+		const body = JSON.parse(response.payload);
+
+		assert.strictEqual(body.code, ERROR_CODES.TRANSACTION_NOT_FOUND);
+	});
+});
