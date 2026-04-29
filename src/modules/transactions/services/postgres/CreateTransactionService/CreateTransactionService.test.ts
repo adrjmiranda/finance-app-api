84ca4bf21a1697f57696c6d5d2a8953f70dbfad8
Adriano Miranda <adrjmiranda@gmail.com>
2026-04-29 11:01:41 -0300

    test: add faker to createtransactionservice integration tests

--- src/modules/transactions/services/postgres/CreateTransactionService/CreateTransactionService.test.ts
+++ src/modules/transactions/services/postgres/CreateTransactionService/CreateTransactionService.test.ts
@@ -3,90 +3,91 @@ import 'reflect-metadata';
 import { describe, test, beforeEach } from 'node:test';
 import assert from 'node:assert';
 import { db } from '#/shared/infra/database/drizzle/db.js';
-import {
-	TRANSACTION_TYPES,
-	transactionsTable,
-} from '#/shared/infra/database/drizzle/schemas/transactions.js';
+import { transactionsTable } from '#/shared/infra/database/drizzle/schemas/transactions.js';
 import { CreateTransactionService } from './CreateTransactionService.js';
 import { container } from 'tsyringe';
 
 import { createUser } from '#/shared/utils/user-helper.js';
-import { randomUUID } from 'node:crypto';
 import { AppError } from '#/shared/error/AppError.js';
 import { ERROR_CODES } from '#/shared/constants/errors/codes/codes.js';
 import { usersTable } from '#/shared/infra/database/drizzle/schemas/users.js';
 import { and, eq } from 'drizzle-orm';
+import { makeTransaction } from '#/shared/tests/factories/make-transaction.js';
 
 describe('CreateTransactionService (Integration)', () => {
-	let createTransactionService: CreateTransactionService;
+  let createTransactionService: CreateTransactionService;
 
-	beforeEach(async () => {
-		await db.delete(usersTable);
+  beforeEach(async () => {
+    await db.delete(usersTable);
 
-		const childContainer = container.createChildContainer();
-		createTransactionService = childContainer.resolve(CreateTransactionService);
-	});
+    const childContainer = container.createChildContainer();
+    createTransactionService = childContainer.resolve(CreateTransactionService);
+  });
 
-	test('should persist a new transaction in the database', async () => {
-		const { user } = await createUser();
+  test('should persist a new transaction in the database', async () => {
+    const { user } = await createUser();
 
-		const executeArguments = {
-			userId: user?.id ?? '',
-			name: 'Test Transaction',
-			date: new Date(),
-			amount: 100.0,
-			type: 'earning' as (typeof TRANSACTION_TYPES)[number],
-		};
+    const fakerTransaction = makeTransaction({ userId: user!.id });
 
-		const { transaction } = await createTransactionService.execute({
-			...executeArguments,
-		});
+    const executeArguments = {
+      userId: fakerTransaction.userId,
+      name: fakerTransaction.name,
+      date: fakerTransaction.date,
+      amount: Number(fakerTransaction.amount),
+      type: fakerTransaction.type,
+    };
 
-		assert.ok(transaction);
+    const { transaction } = await createTransactionService.execute({
+      ...executeArguments,
+    });
 
-		const [transactionInDb] = await db
-			.select()
-			.from(transactionsTable)
-			.where(
-				and(
-					eq(transactionsTable.id, transaction.id),
-					eq(transactionsTable.userId, transaction.userId)
-				)
-			)
-			.limit(1);
+    assert.ok(transaction);
 
-		assert.ok(transactionInDb);
-		assert.strictEqual(transactionInDb.userId, transaction.userId);
-		assert.strictEqual(transactionInDb.name, transaction.name);
-		assert.strictEqual(
-			transactionInDb.date.toISOString(),
-			transaction.date.toISOString()
-		);
-		assert.strictEqual(transactionInDb.amount, transaction.amount);
-		assert.strictEqual(transactionInDb.type, transaction.type);
-	});
+    const [transactionInDb] = await db
+      .select()
+      .from(transactionsTable)
+      .where(
+        and(
+          eq(transactionsTable.id, transaction.id),
+          eq(transactionsTable.userId, transaction.userId)
+        )
+      )
+      .limit(1);
 
-	test('should throw an error if user is not found', async () => {
-		const executeArguments = {
-			userId: randomUUID(),
-			name: 'Test Transaction',
-			date: new Date(),
-			amount: 100.0,
-			type: 'earning' as (typeof TRANSACTION_TYPES)[number],
-		};
+    assert.ok(transactionInDb);
+    assert.strictEqual(transactionInDb.userId, transaction.userId);
+    assert.strictEqual(transactionInDb.name, transaction.name);
+    assert.strictEqual(
+      transactionInDb.date.toISOString(),
+      transaction.date.toISOString()
+    );
+    assert.strictEqual(transactionInDb.amount, transaction.amount);
+    assert.strictEqual(transactionInDb.type, transaction.type);
+  });
 
-		await assert.rejects(
-			async () => {
-				await createTransactionService.execute({
-					...executeArguments,
-				});
-			},
-			(error: unknown) => {
-				assert.ok(error instanceof AppError);
-				assert.strictEqual(error.code, ERROR_CODES.USER_NOT_FOUND);
-				assert.strictEqual(error.status, 404);
-				return true;
-			}
-		);
-	});
+  test('should throw an error if user is not found', async () => {
+    const fakerTransaction = makeTransaction();
+
+    const executeArguments = {
+      userId: fakerTransaction.userId,
+      name: fakerTransaction.name,
+      date: fakerTransaction.date,
+      amount: Number(fakerTransaction.amount),
+      type: fakerTransaction.type,
+    };
+
+    await assert.rejects(
+      async () => {
+        await createTransactionService.execute({
+          ...executeArguments,
+        });
+      },
+      (error: unknown) => {
+        assert.ok(error instanceof AppError);
+        assert.strictEqual(error.code, ERROR_CODES.USER_NOT_FOUND);
+        assert.strictEqual(error.status, 404);
+        return true;
+      }
+    );
+  });
 });
