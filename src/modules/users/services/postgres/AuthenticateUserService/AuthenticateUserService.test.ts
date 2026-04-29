0eb63d13d3c2b9f6b9a96e1d1dd4938aed4b818e
Adriano Miranda <adrjmiranda@gmail.com>
2026-04-29 10:34:51 -0300

    test: add faker to authenticateuserservice integration tests

--- src/modules/users/services/postgres/AuthenticateUserService/AuthenticateUserService.test.ts
+++ src/modules/users/services/postgres/AuthenticateUserService/AuthenticateUserService.test.ts
@@ -11,60 +11,65 @@ import { usersTable } from '#/shared/infra/database/drizzle/schemas/users.js';
 import bcrypt from 'bcrypt';
 import { AppError } from '#/shared/error/AppError.js';
 import { ERROR_CODES } from '#/shared/constants/errors/codes/codes.js';
+import { faker } from '@faker-js/faker';
 
 describe('AuthenticateUserService (Integration)', () => {
-	let authenticateUserService: AuthenticateUserService;
+  let authenticateUserService: AuthenticateUserService;
 
-	beforeEach(async () => {
-		await db.delete(usersTable);
+  beforeEach(async () => {
+    await db.delete(usersTable);
 
-		const childContainer = container.createChildContainer();
-		authenticateUserService = childContainer.resolve(AuthenticateUserService);
-	});
+    const childContainer = container.createChildContainer();
+    authenticateUserService = childContainer.resolve(AuthenticateUserService);
+  });
 
-	test('should authenticate a user', async () => {
-		const email = 'authenticate@test.com';
-		const password = 'password123';
-		const passwordHash = bcrypt.hashSync(password, 10);
+  test('should authenticate a user', async () => {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    const passwordHash = bcrypt.hashSync(password, 10);
 
-		await db.insert(usersTable).values({
-			firstName: 'Adriano',
-			lastName: 'Miranda',
-			email,
-			passwordHash,
-		});
+    await db.insert(usersTable).values({
+      firstName,
+      lastName,
+      email,
+      passwordHash,
+    });
 
-		const { user } = await authenticateUserService.execute({ email, password });
+    const { user } = await authenticateUserService.execute({ email, password });
 
-		assert.ok(user?.id);
-		assert.strictEqual(user?.email, email);
-	});
+    assert.ok(user?.id);
+    assert.strictEqual(user?.email, email);
+  });
 
-	test('should throw an error if password does not match', async () => {
-		const email = 'wrong-pass@test.com';
-		const password = 'correct-password';
-		const passwordHash = bcrypt.hashSync(password, 10);
+  test('should throw an error if password does not match', async () => {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    const passwordHash = bcrypt.hashSync(password, 10);
 
-		await db.insert(usersTable).values({
-			firstName: 'Adriano',
-			lastName: 'Miranda',
-			email,
-			passwordHash,
-		});
+    await db.insert(usersTable).values({
+      firstName,
+      lastName,
+      email,
+      passwordHash,
+    });
 
-		await assert.rejects(
-			async () => {
-				await authenticateUserService.execute({
-					email,
-					password: 'wrong-password',
-				});
-			},
-			(error: unknown) => {
-				assert.ok(error instanceof AppError);
-				assert.strictEqual(error.code, ERROR_CODES.INVALID_CREDENTIALS);
-				assert.strictEqual(error.status, 401);
-				return true;
-			}
-		);
-	});
+    await assert.rejects(
+      async () => {
+        await authenticateUserService.execute({
+          email,
+          password: faker.internet.password(),
+        });
+      },
+      (error: unknown) => {
+        assert.ok(error instanceof AppError);
+        assert.strictEqual(error.code, ERROR_CODES.INVALID_CREDENTIALS);
+        assert.strictEqual(error.status, 401);
+        return true;
+      }
+    );
+  });
 });
