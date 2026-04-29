c12c7b6baa6141efa8e1414b03103153c54cac58
Adriano Miranda <adrjmiranda@gmail.com>
2026-04-29 09:19:47 -0300

    test: refactor deletetransactionservice tests using faker and factory

--- src/modules/transactions/services/postgres/DeleteTransactionService/DeleteTransactionService.spec.ts
+++ src/modules/transactions/services/postgres/DeleteTransactionService/DeleteTransactionService.spec.ts
@@ -5,115 +5,101 @@ import assert from 'node:assert';
 import { DeleteTransactionService } from './DeleteTransactionService.js';
 import { container } from 'tsyringe';
 import { db } from '#/shared/infra/database/drizzle/db.js';
-import { randomUUID } from 'node:crypto';
 import { AppError } from '#/shared/error/AppError.js';
 import { ERROR_CODES } from '#/shared/constants/errors/codes/codes.js';
+import { makeTransaction } from '#/shared/tests/factories/make-transaction.js';
+import { faker } from '@faker-js/faker';
 
 describe('DeleteTransactionService', () => {
-	let deleteTransactionService: DeleteTransactionService;
+  let deleteTransactionService: DeleteTransactionService;
 
-	beforeEach(() => {
-		const childContainer = container.createChildContainer();
-		deleteTransactionService = childContainer.resolve(DeleteTransactionService);
-	});
+  beforeEach(() => {
+    const childContainer = container.createChildContainer();
+    deleteTransactionService = childContainer.resolve(DeleteTransactionService);
+  });
 
-	test('should delete a transaction', async (t) => {
-		const executeArguments = {
-			userId: randomUUID(),
-			transactionId: randomUUID(),
-		};
+  test('should delete a transaction', async (t) => {
+    t.mock.method(db, 'select', () => ({
+      from: () => ({
+        where: () => ({
+          limit: () => Promise.resolve([true]),
+        }),
+      }),
+    }));
 
-		t.mock.method(db, 'select', () => ({
-			from: () => ({
-				where: () => ({
-					limit: () => Promise.resolve([true]),
-				}),
-			}),
-		}));
+    const mockTransaction = makeTransaction();
 
-		const mockTransaction = {
-			id: executeArguments.transactionId,
-			userId: executeArguments.userId,
-			name: 'Test Transaction',
-			date: new Date(),
-			amount: 100.0,
-			type: 'earning',
-			createdAt: new Date(),
-			updatedAt: new Date(),
-		};
+    const deleteMock = t.mock.method(db, 'delete', () => ({
+      where: () => ({
+        returning: () => ({
+          execute: () => Promise.resolve([mockTransaction]),
+        }),
+      }),
+    }));
 
-		const deleteMock = t.mock.method(db, 'delete', () => ({
-			where: () => ({
-				returning: () => ({
-					execute: () => Promise.resolve([mockTransaction]),
-				}),
-			}),
-		}));
+    await deleteTransactionService.execute({
+      userId: mockTransaction.userId,
+      transactionId: mockTransaction.id,
+    });
 
-		await deleteTransactionService.execute({ ...executeArguments });
+    assert.strictEqual(deleteMock.mock.callCount(), 1);
+  });
 
-		assert.strictEqual(deleteMock.mock.callCount(), 1);
-	});
+  test('should throw an error if user is not found', async (t) => {
+    t.mock.method(db, 'select', () => ({
+      from: () => ({
+        where: () => ({
+          limit: () => Promise.resolve([]),
+        }),
+      }),
+    }));
 
-	test('should throw an error if user is not found', async (t) => {
-		const executeArguments = {
-			userId: randomUUID(),
-			transactionId: randomUUID(),
-		};
+    await assert.rejects(
+      async () => {
+        await deleteTransactionService.execute({
+          userId: faker.string.uuid(),
+          transactionId: faker.string.uuid(),
+        });
+      },
+      (error: unknown) => {
+        assert.ok(error instanceof AppError);
+        assert.strictEqual(error.code, ERROR_CODES.USER_NOT_FOUND);
+        assert.strictEqual(error.status, 404);
+        return true;
+      }
+    );
+  });
 
-		t.mock.method(db, 'select', () => ({
-			from: () => ({
-				where: () => ({
-					limit: () => Promise.resolve([]),
-				}),
-			}),
-		}));
+  test('should throw an error if transaction is not found', async (t) => {
+    t.mock.method(db, 'select', () => ({
+      from: () => ({
+        where: () => ({
+          limit: () => Promise.resolve([true]),
+        }),
+      }),
+    }));
 
-		await assert.rejects(
-			async () => {
-				await deleteTransactionService.execute({ ...executeArguments });
-			},
-			(error: unknown) => {
-				assert.ok(error instanceof AppError);
-				assert.strictEqual(error.code, ERROR_CODES.USER_NOT_FOUND);
-				assert.strictEqual(error.status, 404);
-				return true;
-			}
-		);
-	});
+    t.mock.method(db, 'delete', () => ({
+      where: () => ({
+        returning: () => ({
+          execute: () => Promise.resolve([]),
+        }),
+      }),
+    }));
 
-	test('should throw an error if transaction is not found', async (t) => {
-		const executeArguments = {
-			userId: randomUUID(),
-			transactionId: randomUUID(),
-		};
-
-		t.mock.method(db, 'select', () => ({
-			from: () => ({
-				where: () => ({
-					limit: () => Promise.resolve([true]),
-				}),
-			}),
-		}));
-
-		t.mock.method(db, 'delete', () => ({
-			where: () => ({
-				returning: () => ({
-					execute: () => Promise.resolve([]),
-				}),
-			}),
-		}));
-
-		await assert.rejects(
-			async () => {
-				await deleteTransactionService.execute({ ...executeArguments });
-			},
-			(error: unknown) => {
-				assert.ok(error instanceof AppError);
-				assert.strictEqual(error.code, ERROR_CODES.TRANSACTION_NOT_FOUND);
-				assert.strictEqual(error.status, 404);
-				return true;
-			}
-		);
-	});
+    await assert.rejects(
+      async () => {
+        await deleteTransactionService.execute({
+          userId: faker.string.uuid(),
+          transactionId: faker.string.uuid(),
+        });
+      },
+      (error: unknown) => {
+        assert.ok(error instanceof AppError);
+        assert.strictEqual(error.code, ERROR_CODES.TRANSACTION_NOT_FOUND);
+        assert.strictEqual(error.status, 404);
+        return true;
+      }
+    );
+  });
 });
