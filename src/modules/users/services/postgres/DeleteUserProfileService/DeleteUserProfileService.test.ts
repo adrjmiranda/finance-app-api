e1f1315f28f7f4e9fe7d397df7e07d68e220e914
Adriano Miranda <adrjmiranda@gmail.com>
2026-04-29 10:43:07 -0300

    test: add faker to deleteuserservice integration tests

--- src/modules/users/services/postgres/DeleteUserProfileService/DeleteUserProfileService.test.ts
+++ src/modules/users/services/postgres/DeleteUserProfileService/DeleteUserProfileService.test.ts
@@ -9,51 +9,52 @@ import bcrypt from 'bcrypt';
 import { db } from '#/shared/infra/database/drizzle/db.js';
 import { usersTable } from '#/shared/infra/database/drizzle/schemas/users.js';
 import { eq } from 'drizzle-orm';
+import { faker } from '@faker-js/faker';
 
 describe('DeleteUserProfileService (Integration)', async () => {
-	let deleteUserProfileService: DeleteUserProfileService;
+  let deleteUserProfileService: DeleteUserProfileService;
 
-	beforeEach(async () => {
-		await db.delete(usersTable);
+  beforeEach(async () => {
+    await db.delete(usersTable);
 
-		const childContainer = container.createChildContainer();
-		deleteUserProfileService = childContainer.resolve(DeleteUserProfileService);
-	});
+    const childContainer = container.createChildContainer();
+    deleteUserProfileService = childContainer.resolve(DeleteUserProfileService);
+  });
 
-	test('should successfully delete the user profile', async () => {
-		const userData = {
-			firstName: 'Adriano',
-			lastName: 'Miranda',
-			email: 'delete@test.com',
-			password: 'password123',
-		};
+  test('should successfully delete the user profile', async () => {
+    const userData = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
 
-		const passwordHash = await bcrypt.hash(userData.password, 10);
+    const passwordHash = await bcrypt.hash(userData.password, 10);
 
-		const [createdUser] = await db
-			.insert(usersTable)
-			.values({
-				firstName: userData.firstName,
-				lastName: userData.lastName,
-				email: userData.email,
-				passwordHash,
-			})
-			.returning()
-			.execute();
+    const [createdUser] = await db
+      .insert(usersTable)
+      .values({
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        email: userData.email,
+        passwordHash,
+      })
+      .returning()
+      .execute();
 
-		await assert.doesNotReject(async () => {
-			await deleteUserProfileService.execute({
-				userId: createdUser?.id ?? 'not-found',
-				password: userData.password,
-			});
-		});
+    await assert.doesNotReject(async () => {
+      await deleteUserProfileService.execute({
+        userId: createdUser?.id ?? 'not-found',
+        password: userData.password,
+      });
+    });
 
-		const [deletedUser] = await db
-			.select()
-			.from(usersTable)
-			.where(eq(usersTable.id, createdUser?.id ?? 'not-found'))
-			.limit(1);
+    const [deletedUser] = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.id, createdUser?.id ?? 'not-found'))
+      .limit(1);
 
-		assert.ok(!deletedUser?.id);
-	});
+    assert.ok(!deletedUser?.id);
+  });
 });
