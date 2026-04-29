371779fffd8a8cf17fa7f48da20533335e02d584
Adriano Miranda <adrjmiranda@gmail.com>
2026-04-29 10:47:53 -0300

    test: add faker to updateuserpasswordservice integration tests

--- src/modules/users/services/postgres/UpdateUserPasswordService/UpdateUserPasswordService.test.ts
+++ src/modules/users/services/postgres/UpdateUserPasswordService/UpdateUserPasswordService.test.ts
@@ -12,103 +12,104 @@ import bcrypt from 'bcrypt';
 import { eq } from 'drizzle-orm';
 import { AppError } from '#/shared/error/AppError.js';
 import { ERROR_CODES } from '#/shared/constants/errors/codes/codes.js';
+import { faker } from '@faker-js/faker';
 
 describe('UpdateUserPasswordService (Integration)', async () => {
-	let updateUserPasswordService: UpdateUserPasswordService;
+  let updateUserPasswordService: UpdateUserPasswordService;
 
-	beforeEach(async () => {
-		await db.delete(usersTable);
+  beforeEach(async () => {
+    await db.delete(usersTable);
 
-		const childContainer = container.createChildContainer();
-		updateUserPasswordService = childContainer.resolve(
-			UpdateUserPasswordService
-		);
-	});
+    const childContainer = container.createChildContainer();
+    updateUserPasswordService = childContainer.resolve(
+      UpdateUserPasswordService
+    );
+  });
 
-	test('should update the user password', async () => {
-		const oldPassword = 'old-password';
-		const newPassword = 'new-password';
+  test('should update the user password', async () => {
+    const oldPassword = faker.internet.password();
+    const newPassword = faker.internet.password();
 
-		const userData = {
-			firstName: 'Adriano',
-			lastName: 'Miranda',
-			email: 'updatepass@test.com',
-			password: oldPassword,
-		};
+    const userData = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: oldPassword,
+    };
 
-		const oldPasswordHash = await bcrypt.hash(oldPassword, 10);
+    const oldPasswordHash = await bcrypt.hash(oldPassword, 10);
 
-		const [createdUser] = await db
-			.insert(usersTable)
-			.values({
-				firstName: userData.firstName,
-				lastName: userData.lastName,
-				email: userData.email,
-				passwordHash: oldPasswordHash,
-			})
-			.returning()
-			.execute();
+    const [createdUser] = await db
+      .insert(usersTable)
+      .values({
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        email: userData.email,
+        passwordHash: oldPasswordHash,
+      })
+      .returning()
+      .execute();
 
-		await updateUserPasswordService.execute({
-			userId: createdUser?.id ?? 'not-found',
-			oldPassword,
-			newPassword,
-		});
+    await updateUserPasswordService.execute({
+      userId: createdUser?.id ?? 'not-found',
+      oldPassword,
+      newPassword,
+    });
 
-		const [updatedUserWithPassword] = await db
-			.select()
-			.from(usersTable)
-			.where(eq(usersTable.id, createdUser?.id ?? 'not-found'))
-			.limit(1);
+    const [updatedUserWithPassword] = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.id, createdUser?.id ?? 'not-found'))
+      .limit(1);
 
-		assert.ok(updatedUserWithPassword);
+    assert.ok(updatedUserWithPassword);
 
-		const newPasswordIsCorrect = await bcrypt.compare(
-			newPassword,
-			updatedUserWithPassword.passwordHash
-		);
+    const newPasswordIsCorrect = await bcrypt.compare(
+      newPassword,
+      updatedUserWithPassword.passwordHash
+    );
 
-		assert.ok(newPasswordIsCorrect);
-	});
+    assert.ok(newPasswordIsCorrect);
+  });
 
-	test('should throw an error id ols password does not match', async () => {
-		const oldPassword = 'old-password';
-		const newPassword = 'new-password';
+  test('should throw an error id ols password does not match', async () => {
+    const oldPassword = faker.internet.password();
+    const newPassword = faker.internet.password();
 
-		const userData = {
-			firstName: 'Adriano',
-			lastName: 'Miranda',
-			email: 'updatepass@test.com',
-			password: oldPassword,
-		};
+    const userData = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: oldPassword,
+    };
 
-		const oldPasswordHash = await bcrypt.hash(oldPassword, 10);
+    const oldPasswordHash = await bcrypt.hash(oldPassword, 10);
 
-		const [createdUser] = await db
-			.insert(usersTable)
-			.values({
-				firstName: userData.firstName,
-				lastName: userData.lastName,
-				email: userData.email,
-				passwordHash: oldPasswordHash,
-			})
-			.returning()
-			.execute();
+    const [createdUser] = await db
+      .insert(usersTable)
+      .values({
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        email: userData.email,
+        passwordHash: oldPasswordHash,
+      })
+      .returning()
+      .execute();
 
-		await assert.rejects(
-			async () => {
-				await updateUserPasswordService.execute({
-					userId: createdUser?.id ?? 'not-found',
-					oldPassword: 'wrong-old-password',
-					newPassword,
-				});
-			},
-			(error: unknown) => {
-				assert.ok(error instanceof AppError);
-				assert.strictEqual(error.code, ERROR_CODES.INVALID_CREDENTIALS);
-				assert.strictEqual(error.status, 401);
-				return true;
-			}
-		);
-	});
+    await assert.rejects(
+      async () => {
+        await updateUserPasswordService.execute({
+          userId: createdUser?.id ?? 'not-found',
+          oldPassword: faker.internet.password(),
+          newPassword,
+        });
+      },
+      (error: unknown) => {
+        assert.ok(error instanceof AppError);
+        assert.strictEqual(error.code, ERROR_CODES.INVALID_CREDENTIALS);
+        assert.strictEqual(error.status, 401);
+        return true;
+      }
+    );
+  });
 });
