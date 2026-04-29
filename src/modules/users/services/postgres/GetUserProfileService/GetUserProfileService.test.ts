3208489eeb2191b37c067a0afcc8cc945af5fa24
Adriano Miranda <adrjmiranda@gmail.com>
2026-04-29 10:44:32 -0300

    test: add faker to getuserprofileservice integration tests

--- src/modules/users/services/postgres/GetUserProfileService/GetUserProfileService.test.ts
+++ src/modules/users/services/postgres/GetUserProfileService/GetUserProfileService.test.ts
@@ -8,45 +8,46 @@ import { usersTable } from '#/shared/infra/database/drizzle/schemas/users.js';
 import { container } from 'tsyringe';
 
 import bcrypt from 'bcrypt';
+import { faker } from '@faker-js/faker';
 
 describe('GetUserProfileService (Integration)', async () => {
-	let getUserProfileService: GetUserProfileService;
+  let getUserProfileService: GetUserProfileService;
 
-	beforeEach(async () => {
-		await db.delete(usersTable);
+  beforeEach(async () => {
+    await db.delete(usersTable);
 
-		const childContainer = container.createChildContainer();
-		getUserProfileService = childContainer.resolve(GetUserProfileService);
-	});
+    const childContainer = container.createChildContainer();
+    getUserProfileService = childContainer.resolve(GetUserProfileService);
+  });
 
-	test('should successfully get the user profile', async () => {
-		const userData = {
-			firstName: 'Adriano',
-			lastName: 'Miranda',
-			email: 'userprofile@test.com',
-			password: 'password123',
-		};
+  test('should successfully get the user profile', async () => {
+    const userData = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
 
-		const passwordHash = await bcrypt.hash(userData.password, 10);
+    const passwordHash = await bcrypt.hash(userData.password, 10);
 
-		const [createdUser] = await db
-			.insert(usersTable)
-			.values({
-				firstName: userData.firstName,
-				lastName: userData.lastName,
-				email: userData.email,
-				passwordHash,
-			})
-			.returning()
-			.execute();
+    const [createdUser] = await db
+      .insert(usersTable)
+      .values({
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        email: userData.email,
+        passwordHash,
+      })
+      .returning()
+      .execute();
 
-		const { user } = await getUserProfileService.execute({
-			userId: createdUser?.id ?? 'not-found',
-		});
+    const { user } = await getUserProfileService.execute({
+      userId: createdUser?.id ?? 'not-found',
+    });
 
-		assert.ok(user?.id);
-		assert.strictEqual(user.firstName, userData.firstName);
-		assert.strictEqual(user.lastName, userData.lastName);
-		assert.strictEqual(user.email, userData.email);
-	});
+    assert.ok(user?.id);
+    assert.strictEqual(user.firstName, userData.firstName);
+    assert.strictEqual(user.lastName, userData.lastName);
+    assert.strictEqual(user.email, userData.email);
+  });
 });
