03de20368b2db59d5785526d5a15b758e93ad9d9
Adriano Miranda <adrjmiranda@gmail.com>
2026-04-29 09:16:27 -0300

    test: refactor createtransactionservice tests using faker and factory

--- src/modules/transactions/services/postgres/CreateTransactionService/CreateTransactionService.spec.ts
+++ src/modules/transactions/services/postgres/CreateTransactionService/CreateTransactionService.spec.ts
@@ -5,92 +5,78 @@ import assert from 'node:assert';
 import { CreateTransactionService } from './CreateTransactionService.js';
 import { container } from 'tsyringe';
 import { db } from '#/shared/infra/database/drizzle/db.js';
-import { TRANSACTION_TYPES } from '#/shared/infra/database/drizzle/schemas/transactions.js';
-import { randomUUID } from 'node:crypto';
 import { AppError } from '#/shared/error/AppError.js';
 import { ERROR_CODES } from '#/shared/constants/errors/codes/codes.js';
 
+import { makeTransaction } from '#/shared/tests/factories/make-transaction.js';
+
 describe('CreateTransactionService', () => {
-	let createTransactionService: CreateTransactionService;
+  let createTransactionService: CreateTransactionService;
 
-	beforeEach(() => {
-		const childContainer = container.createChildContainer();
-		createTransactionService = childContainer.resolve(CreateTransactionService);
-	});
+  beforeEach(() => {
+    const childContainer = container.createChildContainer();
+    createTransactionService = childContainer.resolve(CreateTransactionService);
+  });
 
-	test('should create a new transaction', async (t) => {
-		const transactionData = {
-			userId: randomUUID(),
-			name: 'Test Transaction',
-			date: new Date(),
-			amount: 100.0,
-			type: TRANSACTION_TYPES[0],
-		};
+  test('should create a new transaction', async (t) => {
+    t.mock.method(db, 'select', () => ({
+      from: () => ({
+        where: () => ({
+          limit: () => Promise.resolve([true]),
+        }),
+      }),
+    }));
 
-		t.mock.method(db, 'select', () => ({
-			from: () => ({
-				where: () => ({
-					limit: () => Promise.resolve([true]),
-				}),
-			}),
-		}));
+    const mockCreatedTransaction = makeTransaction();
 
-		const mockCreatedTransaction = {
-			...transactionData,
-			id: randomUUID(),
-			createdAt: new Date(),
-			updatedAt: new Date(),
-		};
+    t.mock.method(db, 'insert', () => ({
+      values: () => ({
+        returning: () => ({
+          execute: () => Promise.resolve([mockCreatedTransaction]),
+        }),
+      }),
+    }));
 
-		t.mock.method(db, 'insert', () => ({
-			values: () => ({
-				returning: () => ({
-					execute: () => Promise.resolve([mockCreatedTransaction]),
-				}),
-			}),
-		}));
+    const { transaction } = await createTransactionService.execute({
+      name: mockCreatedTransaction.name,
+      date: mockCreatedTransaction.date,
+      amount: Number(mockCreatedTransaction.amount),
+      userId: mockCreatedTransaction.userId,
+      type: mockCreatedTransaction.type,
+    });
 
-		const { transaction } = await createTransactionService.execute({
-			...transactionData,
-		});
+    assert.strictEqual(transaction?.id, mockCreatedTransaction.id);
+    assert.strictEqual(transaction?.userId, mockCreatedTransaction.userId);
+    assert.strictEqual(transaction?.name, mockCreatedTransaction.name);
+    assert.strictEqual(transaction?.date, mockCreatedTransaction.date);
+    assert.strictEqual(transaction?.amount, mockCreatedTransaction.amount);
+    assert.strictEqual(transaction?.type, mockCreatedTransaction.type);
+  });
 
-		assert.strictEqual(transaction?.id, mockCreatedTransaction.id);
-		assert.strictEqual(transaction?.userId, mockCreatedTransaction.userId);
-		assert.strictEqual(transaction?.name, mockCreatedTransaction.name);
-		assert.strictEqual(transaction?.date, mockCreatedTransaction.date);
-		assert.strictEqual(transaction?.amount, mockCreatedTransaction.amount);
-		assert.strictEqual(transaction?.type, mockCreatedTransaction.type);
-	});
+  test('should throw an error if user is not found', async (t) => {
+    t.mock.method(db, 'select', () => ({
+      from: () => ({
+        where: () => ({
+          limit: () => Promise.resolve([]),
+        }),
+      }),
+    }));
 
-	test('should throw an error if user is not found', async (t) => {
-		const transactionData = {
-			userId: randomUUID(),
-			name: 'Test Transaction',
-			date: new Date(),
-			amount: 100.0,
-			type: TRANSACTION_TYPES[0],
-		};
+    const { userId, name, date, amount, type } = makeTransaction();
+    const mockPayload = { userId, name, date, amount: Number(amount), type };
 
-		t.mock.method(db, 'select', () => ({
-			from: () => ({
-				where: () => ({
-					limit: () => Promise.resolve([]),
-				}),
-			}),
-		}));
-
-		await assert.rejects(
-			async () => {
-				await createTransactionService.execute({
-					...transactionData,
-				});
-			},
-			(error: unknown) => {
-				assert.ok(error instanceof AppError);
-				assert.strictEqual(error.code, ERROR_CODES.USER_NOT_FOUND);
-				assert.strictEqual(error.status, 404);
-				return true;
-			}
-		);
-	});
+    await assert.rejects(
+      async () => {
+        await createTransactionService.execute({
+          ...mockPayload,
+        });
+      },
+      (error: unknown) => {
+        assert.ok(error instanceof AppError);
+        assert.strictEqual(error.code, ERROR_CODES.USER_NOT_FOUND);
+        assert.strictEqual(error.status, 404);
+        return true;
+      }
+    );
+  });
 });
