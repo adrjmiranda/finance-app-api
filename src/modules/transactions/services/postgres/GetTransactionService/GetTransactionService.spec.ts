adec0f7662da14c8b59258e43872a4918666408e
Adriano Miranda <adrjmiranda@gmail.com>
2026-04-29 09:33:04 -0300

    test: refactor gettransactionservice tests using faker and factory

--- src/modules/transactions/services/postgres/GetTransactionService/GetTransactionService.spec.ts
+++ src/modules/transactions/services/postgres/GetTransactionService/GetTransactionService.spec.ts
@@ -5,115 +5,99 @@ import assert from 'node:assert';
 import { GetTransactionService } from './GetTransactionService.js';
 import { container } from 'tsyringe';
 import { db } from '#/shared/infra/database/drizzle/db.js';
-import { randomUUID } from 'node:crypto';
 import { AppError } from '#/shared/error/AppError.js';
 import { ERROR_CODES } from '#/shared/constants/errors/codes/codes.js';
+import { makeTransaction } from '#/shared/tests/factories/make-transaction.js';
+import { faker } from '@faker-js/faker';
 
 describe('GetTransactionService', () => {
-	let getTransactionService: GetTransactionService;
+  let getTransactionService: GetTransactionService;
 
-	beforeEach(() => {
-		const childContainer = container.createChildContainer();
-		getTransactionService = childContainer.resolve(GetTransactionService);
-	});
+  beforeEach(() => {
+    const childContainer = container.createChildContainer();
+    getTransactionService = childContainer.resolve(GetTransactionService);
+  });
 
-	test('should get a transaction', async (t) => {
-		const mockTransaction = {
-			id: randomUUID(),
-			userId: randomUUID(),
-			name: 'Test Transaction',
-			date: new Date(),
-			amount: 100.0,
-			type: 'earning',
-			createdAt: new Date(),
-			updatedAt: new Date(),
-		};
+  test('should get a transaction', async (t) => {
+    const mockTransaction = makeTransaction();
 
-		const selectMock = t.mock.method(db, 'select', () => ({
-			from: () => ({
-				where: () => ({
-					limit: () => {
-						if (selectMock.mock.callCount() === 1) {
-							return Promise.resolve([{ id: mockTransaction.userId }]);
-						}
+    const selectMock = t.mock.method(db, 'select', () => ({
+      from: () => ({
+        where: () => ({
+          limit: () => {
+            if (selectMock.mock.callCount() === 1) {
+              return Promise.resolve([{ id: mockTransaction.userId }]);
+            }
 
-						return Promise.resolve([mockTransaction]);
-					},
-				}),
-			}),
-		}));
+            return Promise.resolve([mockTransaction]);
+          },
+        }),
+      }),
+    }));
 
-		const { transaction } = await getTransactionService.execute({
-			userId: mockTransaction.userId,
-			transactionId: mockTransaction.id,
-		});
+    const { transaction } = await getTransactionService.execute({
+      userId: mockTransaction.userId,
+      transactionId: mockTransaction.id,
+    });
 
-		assert.strictEqual(selectMock.mock.callCount(), 2);
-		assert.strictEqual(transaction?.id, mockTransaction.id);
-		assert.strictEqual(transaction?.userId, mockTransaction.userId);
-	});
+    assert.strictEqual(selectMock.mock.callCount(), 2);
+    assert.strictEqual(transaction?.id, mockTransaction.id);
+    assert.strictEqual(transaction?.userId, mockTransaction.userId);
+  });
 
-	test('should throw an error if user is not found', async (t) => {
-		const selectMock = t.mock.method(db, 'select', () => ({
-			from: () => ({
-				where: () => ({
-					limit: () => {
-						if (selectMock.mock.callCount() === 1) {
-							return Promise.resolve([]);
-						}
+  test('should throw an error if user is not found', async (t) => {
+    t.mock.method(db, 'select', () => ({
+      from: () => ({
+        where: () => ({
+          limit: () => Promise.resolve([]),
+        }),
+      }),
+    }));
 
-						return Promise.resolve([true]);
-					},
-				}),
-			}),
-		}));
+    await assert.rejects(
+      async () => {
+        await getTransactionService.execute({
+          userId: faker.string.uuid(),
+          transactionId: faker.string.uuid(),
+        });
+      },
+      (error: unknown) => {
+        assert.ok(error instanceof AppError);
+        assert.strictEqual(error.code, ERROR_CODES.USER_NOT_FOUND);
+        assert.strictEqual(error.status, 404);
+        return true;
+      }
+    );
+  });
 
-		await assert.rejects(
-			async () => {
-				await getTransactionService.execute({
-					userId: randomUUID(),
-					transactionId: randomUUID(),
-				});
-			},
-			(error: unknown) => {
-				assert.ok(error instanceof AppError);
-				assert.strictEqual(error.code, ERROR_CODES.USER_NOT_FOUND);
-				assert.strictEqual(error.status, 404);
-				return true;
-			}
-		);
-	});
+  test('should throw an error if transaction is not found', async (t) => {
+    const selectMock = t.mock.method(db, 'select', () => ({
+      from: () => ({
+        where: () => ({
+          limit: () => {
+            if (selectMock.mock.callCount() === 1) {
+              return Promise.resolve([true]);
+            }
 
-	test('should throw an error if transaction is not found', async (t) => {
-		const executeArguments = {
-			transactionId: randomUUID(),
-			userId: randomUUID(),
-		};
+            return Promise.resolve([]);
+          },
+        }),
+      }),
+    }));
 
-		const selectMock = t.mock.method(db, 'select', () => ({
-			from: () => ({
-				where: () => ({
-					limit: () => {
-						if (selectMock.mock.callCount() === 1) {
-							return Promise.resolve([true]);
-						}
-
-						return Promise.resolve([]);
-					},
-				}),
-			}),
-		}));
-
-		await assert.rejects(
-			async () => {
-				await getTransactionService.execute({ ...executeArguments });
-			},
-			(error: unknown) => {
-				assert.ok(error instanceof AppError);
-				assert.strictEqual(error.code, ERROR_CODES.TRANSACTION_NOT_FOUND);
-				assert.strictEqual(error.status, 404);
-				return true;
-			}
-		);
-	});
+    await assert.rejects(
+      async () => {
+        await getTransactionService.execute({
+          userId: faker.string.uuid(),
+          transactionId: faker.string.uuid(),
+        });
+      },
+      (error: unknown) => {
+        assert.ok(error instanceof AppError);
+        assert.strictEqual(error.code, ERROR_CODES.TRANSACTION_NOT_FOUND);
+        assert.strictEqual(error.status, 404);
+        return true;
+      }
+    );
+  });
 });
