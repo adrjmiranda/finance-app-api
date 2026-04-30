9ee70d3a42de7cbdab687fcd1c7d586e48f5d107
Adriano Miranda <adrjmiranda@gmail.com>
2026-04-29 22:21:19 -0300

    test: implement dynamic data using faker in createusercontroller

--- src/modules/users/infra/http/controllers/CreateUserController/CreateUserController.spec.ts
+++ src/modules/users/infra/http/controllers/CreateUserController/CreateUserController.spec.ts
@@ -8,121 +8,104 @@ import { container } from 'tsyringe';
 import { createUserBodySchema } from '#/modules/users/schemas/requests/body/create-user-body-schema.js';
 import { CreateUserController } from './CreateUserController.js';
 import { CreateUserService } from '#/modules/users/services/postgres/CreateUserService/CreateUserService.js';
-
-import {
-	createMockReply,
-	createMockRequest,
-} from '#/test/utils/fastify-mock.js';
+import { createMockHttpRequest } from '#/test/utils/http-mock.js';
+import { makeUser } from '#/shared/tests/factories/make-user.js';
+import { faker } from '@faker-js/faker';
 
 describe('CreateUserController', () => {
-	let createUserController: CreateUserController;
-	let createUserService: CreateUserService;
+  let createUserController: CreateUserController;
+  let createUserService: CreateUserService;
 
-	beforeEach(() => {
-		container.clearInstances();
-		const childContainer = container.createChildContainer();
+  beforeEach(() => {
+    container.clearInstances();
+    const childContainer = container.createChildContainer();
 
-		createUserService = {
-			execute: async () => ({
-				user: undefined,
-			}),
-		} as CreateUserService;
+    createUserService = {
+      execute: async () => ({
+        user: undefined,
+      }),
+    } as CreateUserService;
 
-		childContainer.registerInstance(CreateUserService, createUserService);
-		createUserController = childContainer.resolve(CreateUserController);
-	});
+    childContainer.registerInstance(CreateUserService, createUserService);
+    createUserController = childContainer.resolve(CreateUserController);
+  });
 
-	test('should create a new user', async (t) => {
-		const userPayload = {
-			firstName: 'John',
-			lastName: 'Doe',
-			email: 'john.doe@example.com',
-			password: 'password123',
-		};
+  test('should create a new user', async (t) => {
+    const userData = makeUser();
 
-		const userData = {
-			...userPayload,
-			id: 'uuid-v4',
-			createdAt: new Date(),
-			updatedAt: new Date(),
-		};
+    const mockHttoRequest = createMockHttpRequest();
 
-		const mockRequest = createMockRequest({
-			body: userPayload,
-		});
-		const mockReply = createMockReply(t);
+    t.mock.method(createUserBodySchema, 'parse', () => ({
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      email: userData.email,
+      password: faker.internet.password(),
+    }));
+    t.mock.method(createUserService, 'execute', async () => ({
+      user: userData,
+    }));
 
-		t.mock.method(createUserBodySchema, 'parse', () => userPayload);
-		t.mock.method(createUserService, 'execute', async () => ({
-			user: userData,
-		}));
+    const response = await createUserController.handle(mockHttoRequest);
+    const { user } = response.body as {
+      user: Omit<typeof userData, 'passwordHash'>;
+    };
 
-		await createUserController.handle(mockRequest, mockReply);
+    assert.strictEqual(response.statusCode, 201);
+    assert.deepEqual(user, userData as Omit<typeof userData, 'passwordHash'>);
+  });
 
-		assert.strictEqual(mockReply.status.mock.calls[0]?.arguments[0], 201);
-		assert.deepStrictEqual(mockReply.send.mock.calls[0]?.arguments[0], {
-			user: userData,
-		});
-	});
+  test('should throw an error if service fails', async (t) => {
+    const userData = makeUser();
 
-	test('should throw an error if service fails', async (t) => {
-		const userPayload = {
-			firstName: 'John',
-			lastName: 'Doe',
-			email: 'john.doe@example.com',
-			password: 'password123',
-		};
+    const mockHttoRequest = createMockHttpRequest();
 
-		const mockRequest = createMockRequest({
-			body: userPayload,
-		});
-		const mockReply = createMockReply(t);
+    t.mock.method(createUserBodySchema, 'parse', () => ({
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      email: userData.email,
+      password: faker.internet.password(),
+    }));
 
-		t.mock.method(createUserBodySchema, 'parse', () => userPayload);
+    t.mock.method(createUserService, 'execute', async () => {
+      throw new Error('Service error');
+    });
 
-		t.mock.method(createUserService, 'execute', async () => {
-			throw new Error('Service error');
-		});
+    await assert.rejects(
+      async () => {
+        await createUserController.handle(mockHttoRequest);
+      },
+      {
+        name: 'Error',
+        message: 'Service error',
+      }
+    );
+  });
 
-		await assert.rejects(
-			async () => {
-				await createUserController.handle(mockRequest, mockReply);
-			},
-			{
-				name: 'Error',
-				message: 'Service error',
-			}
-		);
-	});
+  test('should throw an error if body is invalid', async (t) => {
+    const mockHttoRequest = createMockHttpRequest();
 
-	test('should throw an error if body is invalid', async (t) => {
-		const mockRequest = createMockRequest({
-			body: {},
-		});
-		const mockReply = createMockReply(t);
+    t.mock.method(createUserBodySchema, 'parse', () => {
+      throw new Error('Validation error');
+    });
 
-		t.mock.method(createUserBodySchema, 'parse', () => {
-			throw new Error('Validation error');
-		});
+    await assert.rejects(
+      async () => {
+        await createUserController.handle(mockHttoRequest);
+      },
+      {
+        name: 'Error',
+        message: 'Validation error',
+      }
+    );
 
-		await assert.rejects(
-			async () => {
-				await createUserController.handle(mockRequest, mockReply);
-			},
-			{
-				name: 'Error',
-				message: 'Validation error',
-			}
-		);
+    const executeMock = t.mock.method(
+      createUserService,
+      'execute',
+      async () => {
+        throw new Error('Service error');
+      }
+    );
 
-		const executeMock = t.mock.method(
-			createUserService,
-			'execute',
-			async () => {
-				throw new Error('Service error');
-			}
-		);
-
-		assert.strictEqual(executeMock.mock.callCount(), 0);
-	});
+    assert.strictEqual(executeMock.mock.callCount(), 0);
+  });
 });
