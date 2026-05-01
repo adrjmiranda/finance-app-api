adacd7ec92a6cc155d1012fe67ad8d0bae38d61f
Adriano Miranda <adrjmiranda@gmail.com>
2026-05-01 09:07:40 -0300

    test: use faker in gettransactioncontroller integration test

--- src/modules/transactions/infra/http/controllers/GetTransactionController/GetTransactionController.test.ts
+++ src/modules/transactions/infra/http/controllers/GetTransactionController/GetTransactionController.test.ts
@@ -6,66 +6,71 @@ import { db } from '#/shared/infra/database/drizzle/db.js';
 import { usersTable } from '#/shared/infra/database/drizzle/schemas/users.js';
 import { createAndAuthenticateUser } from '#/shared/utils/authenticate-user-helper.js';
 import { app } from '#/shared/infra/http/app.js';
-import { transactionsTable } from '#/shared/infra/database/drizzle/schemas/transactions.js';
-import { randomUUID } from 'node:crypto';
+import {
+  TRANSACTION_TYPES,
+  transactionsTable,
+} from '#/shared/infra/database/drizzle/schemas/transactions.js';
 import { ERROR_CODES } from '#/shared/constants/errors/codes/codes.js';
+import { faker } from '@faker-js/faker';
 
 describe('GetTransactionController (Integration)', () => {
-	beforeEach(async () => {
-		await db.delete(usersTable);
-	});
+  beforeEach(async () => {
+    await db.delete(usersTable);
+  });
 
-	test('should get a transaction', async () => {
-		const { authenticatedUser, token } = await createAndAuthenticateUser(app);
+  test('should get a transaction', async () => {
+    const { authenticatedUser, token } = await createAndAuthenticateUser(app);
 
-		const [createdTransaction] = await db
-			.insert(transactionsTable)
-			.values({
-				name: 'Pizza',
-				date: new Date(),
-				amount: String(62.0),
-				type: 'expense',
-				userId: authenticatedUser!.id,
-			})
-			.returning()
-			.execute();
+    const [createdTransaction] = await db
+      .insert(transactionsTable)
+      .values({
+        name: faker.string.alpha(16),
+        date: new Date(),
+        amount: String(faker.number.float({ fractionDigits: 2 })),
+        type: faker.helpers.arrayElement(TRANSACTION_TYPES),
+        userId: authenticatedUser!.id,
+      })
+      .returning()
+      .execute();
 
-		const transactionId = createdTransaction!.id;
+    const transactionId = createdTransaction!.id;
 
-		const response = await app.inject({
-			method: 'GET',
-			url: `/transactions/${transactionId}`,
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		});
+    const response = await app.inject({
+      method: 'GET',
+      url: `/transactions/${transactionId}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-		const body = JSON.parse(response.payload);
+    const body = JSON.parse(response.payload);
 
-		assert.strictEqual(response.statusCode, 200);
+    assert.strictEqual(response.statusCode, 200);
 
-		assert.ok(body.transaction);
+    assert.ok(body.transaction);
 
-		assert.strictEqual(body.transaction.id, transactionId);
-		assert.strictEqual(body.transaction.userId, authenticatedUser!.id);
-		assert.strictEqual(body.transaction.name, 'Pizza');
-	});
+    assert.strictEqual(body.transaction.id, transactionId);
+    assert.strictEqual(body.transaction.userId, authenticatedUser!.id);
+    assert.strictEqual(body.transaction.name, createdTransaction!.name);
+  });
 
-	test('should throw an error if transaction does not exist', async () => {
-		const { token } = await createAndAuthenticateUser(app);
+  test('should throw an error if transaction does not exist', async () => {
+    const { token } = await createAndAuthenticateUser(app);
 
-		const response = await app.inject({
-			method: 'GET',
-			url: `/transactions/${randomUUID()}`,
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		});
+    const wrongTransactionId = faker.string.uuid();
 
-		assert.strictEqual(response.statusCode, 404);
+    const response = await app.inject({
+      method: 'GET',
+      url: `/transactions/${wrongTransactionId}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-		const body = JSON.parse(response.payload);
+    assert.strictEqual(response.statusCode, 404);
 
-		assert.strictEqual(body.code, ERROR_CODES.TRANSACTION_NOT_FOUND);
-	});
+    const body = JSON.parse(response.payload);
+
+    assert.strictEqual(body.code, ERROR_CODES.TRANSACTION_NOT_FOUND);
+  });
 });
