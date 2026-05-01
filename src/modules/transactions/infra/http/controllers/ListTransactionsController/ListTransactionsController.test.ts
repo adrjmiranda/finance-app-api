f832d8b3861e3ef77f68964c21fd201a31d8689f
Adriano Miranda <adrjmiranda@gmail.com>
2026-05-01 09:14:59 -0300

    test: use faker in listtransactionscontroller integration test

--- src/modules/transactions/infra/http/controllers/ListTransactionsController/ListTransactionsController.test.ts
+++ src/modules/transactions/infra/http/controllers/ListTransactionsController/ListTransactionsController.test.ts
@@ -7,73 +7,74 @@ import { usersTable } from '#/shared/infra/database/drizzle/schemas/users.js';
 import { createAndAuthenticateUser } from '#/shared/utils/authenticate-user-helper.js';
 import { app } from '#/shared/infra/http/app.js';
 import {
-	TRANSACTION_TYPES,
-	transactionsTable,
+  TRANSACTION_TYPES,
+  transactionsTable,
 } from '#/shared/infra/database/drizzle/schemas/transactions.js';
+import { faker } from '@faker-js/faker';
 
 describe('ListTransactionsController (Integration)', () => {
-	beforeEach(async () => {
-		await db.delete(usersTable);
-	});
+  beforeEach(async () => {
+    await db.delete(usersTable);
+  });
 
-	test('should list transactions', async () => {
-		const { authenticatedUser, token } = await createAndAuthenticateUser(app);
+  test('should list transactions', async () => {
+    const { authenticatedUser, token } = await createAndAuthenticateUser(app);
 
-		const earning = 150.0;
-		const expense = 62.0;
-		const investment = 220.0;
+    const earning = faker.number.float({ fractionDigits: 2 });
+    const expense = faker.number.float({ fractionDigits: 2 });
+    const investment = faker.number.float({ fractionDigits: 2 });
 
-		const transactionsData = [
-			{
-				name: 'Transaction 1',
-				date: new Date(),
-				amount: String(expense),
-				type: 'expense' as (typeof TRANSACTION_TYPES)[number],
-				userId: authenticatedUser!.id,
-			},
-			{
-				name: 'Transaction 2',
-				date: new Date(),
-				amount: String(earning),
-				type: 'earning' as (typeof TRANSACTION_TYPES)[number],
-				userId: authenticatedUser!.id,
-			},
-			{
-				name: 'Transaction 3',
-				date: new Date(),
-				amount: String(investment),
-				type: 'investment' as (typeof TRANSACTION_TYPES)[number],
-				userId: authenticatedUser!.id,
-			},
-		];
+    const transactionsData = [
+      {
+        name: faker.string.alpha(16),
+        date: new Date(),
+        amount: String(expense),
+        type: 'expense' as (typeof TRANSACTION_TYPES)[number],
+        userId: authenticatedUser!.id,
+      },
+      {
+        name: faker.string.alpha(16),
+        date: new Date(),
+        amount: String(earning),
+        type: 'earning' as (typeof TRANSACTION_TYPES)[number],
+        userId: authenticatedUser!.id,
+      },
+      {
+        name: faker.string.alpha(16),
+        date: new Date(),
+        amount: String(investment),
+        type: 'investment' as (typeof TRANSACTION_TYPES)[number],
+        userId: authenticatedUser!.id,
+      },
+    ];
 
-		await db.insert(transactionsTable).values(transactionsData);
+    await db.insert(transactionsTable).values(transactionsData);
 
-		const response = await app.inject({
-			method: 'GET',
-			url: '/transactions',
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		});
+    const response = await app.inject({
+      method: 'GET',
+      url: '/transactions',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-		assert.strictEqual(response.statusCode, 200);
+    assert.strictEqual(response.statusCode, 200);
 
-		const body = JSON.parse(response.payload);
+    const body = JSON.parse(response.payload);
 
-		assert.strictEqual(body.transactions.length, transactionsData.length);
+    assert.strictEqual(body.transactions.length, transactionsData.length);
 
-		assert.deepStrictEqual(
-			body.transactions[0].name,
-			transactionsData[0]?.name
-		);
-		assert.deepStrictEqual(
-			body.transactions[1].name,
-			transactionsData[1]?.name
-		);
-		assert.deepStrictEqual(
-			body.transactions[2].name,
-			transactionsData[2]?.name
-		);
-	});
+    assert.deepStrictEqual(
+      body.transactions[0].name,
+      transactionsData[0]?.name
+    );
+    assert.deepStrictEqual(
+      body.transactions[1].name,
+      transactionsData[1]?.name
+    );
+    assert.deepStrictEqual(
+      body.transactions[2].name,
+      transactionsData[2]?.name
+    );
+  });
 });
